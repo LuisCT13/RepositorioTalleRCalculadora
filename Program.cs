7f21bed954ce3c187fc58d31b3765ac5bcfd63ff
luis <alumno@PC-LABA-25>
2023-10-06 10:52:28 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este programa esta hecho el sugundo dia del simposio de ciencias e inteligencia artificial de la ingieneria en sistemas computacionales");
+            Console.WriteLine(  "esta bien chido el taller!!!");
+            Console.WriteLine(  "wenas wenas ");
+            Console.WriteLine(  "ola ");
+            Console.WriteLine(  "cambios ");
+            Console.WriteLine("pq no se guardan ahhhhhhhhhhhhhhhhhhh");
+            Console.WriteLine(  "bskihds");
+            //hgfjgh
+        }
+    }
+}
